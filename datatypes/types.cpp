368e2bf248144aef019440c91ef601c0be5a9852
Vamsi995 <saivamsi.ds123@gmail.com>
2020-11-02 18:52:13 +0530

    solved the second question

---
+++ datatypes/types.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+
+	int a;
+	long int b;
+	char c;
+	float d;
+	double e;
+
+	cin >> a >> b >> c >> d >> e;
+
+//	cout << a << b << c << d << e << endl;
+
+
+	printf("%d\n",a);
+
+	printf("%ld\n",b);
+
+	printf("%c\n",c);
+
+	printf("%.3f\n",d);
+
+	printf("%.9lf\n",e);
+
+	return 0;
+
+}
+
